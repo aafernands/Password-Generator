556681ef24ff314af51e6633ee699b5fe1ba75e7
Alex Fernandes <alexfernands@outlook.com>
2020-09-30 20:57:34 -0400

    update

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
+function generateNewPassword() {
+	var length = Number(prompt('How many characters would you like your password to be?'));
 
+	if (isNaN(length) || length < 8 || length > 128) {
+		alert("The length doesn't meet the requirements");
+		return;
+	}
 
+	var symbols = confirm('Would you like to use special characters?');
+	var uppers = confirm('Would you like to use uppercase letters?');
+	var lowers = confirm('Would you like to use lowercase letters?');
+	var numbers = confirm('Would you like to use numbers?');
 
+	if (uppers) {
+		password = password + 'QWERTYUIOPASDFGHJKLZXCVBNM';
+	}
 
+	if (lowers) {
+		password = password + 'qwertyuiopasdfghjklzxcvbnm';
+	}
+	if (numbers) {
+		password = password + '1234567890';
+	}
 
+	if (symbols) {
+		password = password + '!@#$%^&*(){}[]=<>/,.';
+	}
 
+	for (var i = 0; i < length; i++) {
+		var password = Math.floor(Math.random() * length) + 1;
 
-var messageLength = 148;
-function myFunction() {
-	var message = prompt('Write a message:');
-	var charactersCount = message.length;
-	alert(`You have written ${message.length} characters, you have ${148 - message.length} characters remaining.`);
-	document.getElementById('yourMessage').innerHTML = message
-	document.getElementById('charactersCount').innerHTML = charactersCount
-	document.getElementById('messageLength').innerHTML = messageLength
-	document.getElementById('remainingCharacters').innerHTML = charactersCount - 148
+		document.getElementById('displayPassword').innerHTML = password;
+
+		console.log(password);
+	}
 }
