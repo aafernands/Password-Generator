ee8b34418fc0ce9d09978640fbaddeb91011b1ee
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2024-12-31 00:44:00 -0500

    Updates to replace dialogs to inputs index.html

--- script.js
+++ script.js
@@ -1,60 +1,58 @@
 var alertBox = document.querySelector('.alertBox');
 
 function generateNewPassword() {
-	var length = document.getElementById("lenght")
-	lenght = lenght.value
-	if (isNaN(length) || length < 8 || length > 128) {
-		alert("The length doesn't meet the requirements");
-		return;
-	}
-	var symbols = confirm('Would you like to use special characters?');
-	var uppers = confirm('Would you like to use uppercase letters?');
-	var lowers = confirm('Would you like to use lowercase letters?');
-	var numbers = confirm('Would you like to use numbers?');
-	var password = '';
+    var length = document.getElementById("lenght").value;
+    if (isNaN(length) || length < 8 || length > 128) {
+        alert("The length doesn't meet the requirements");
+        return;
+    }
 
-	if (uppers) {
-		password = password + 'QWERTYUIOPASDFGHJKLZXCVBNM';
-	}
+    var symbols = document.getElementById('symbols').checked;
+    var uppers = document.getElementById('uppers').checked;
+    var lowers = document.getElementById('lowers').checked;
+    var numbers = document.getElementById('numbers').checked;
+    var password = '';
 
-	if (lowers) {
-		password = password + 'qwertyuiopasdfghjklzxcvbnm';
-	}
+    if (uppers) {
+        password += 'QWERTYUIOPASDFGHJKLZXCVBNM';
+    }
 
-	if (numbers) {
-		password = password + '1234567890';
-	}
+    if (lowers) {
+        password += 'qwertyuiopasdfghjklzxcvbnm';
+    }
 
-	if (symbols) {
-		password = password + '!@#$%^&*(){}[]=<>/,.';
-	}
+    if (numbers) {
+        password += '1234567890';
+    }
 
-	if (!symbols && !numbers && !uppers && !lowers) {
-		choices = alert('You must choose a character type!');
-	}
+    if (symbols) {
+        password += '!@#$%^&*(){}[]=<>/,.';
+    }
 
-	var randomPassword = '';
-	for (var i = 0; i < length; i++) {
-		randomPassword += password.charAt(Math.floor(Math.random() * password.length));
-	}
+    if (!symbols && !numbers && !uppers && !lowers) {
+        alert('You must choose at least one character type!');
+        return;
+    }
 
-	document.getElementById('displayPassword').innerHTML = randomPassword;
-	alertBox.innerHTML = 'Your New Password Copied: <br>' + randomPassword;
+    var randomPassword = '';
+    for (var i = 0; i < length; i++) {
+        randomPassword += password.charAt(Math.floor(Math.random() * password.length));
+    }
+
+    document.getElementById('displayPassword').value = randomPassword;
+    alertBox.innerHTML = 'Your New Password Copied: <br>' + randomPassword;
 }
 
 function copyPassword() {
-	var copyPassText = document.getElementById('displayPassword');
-	copyPassText.select();
-	copyPassText.setSelectionRange(0, 9999);
-	document.execCommand('copy');
-	alertBox.classList.toggle('active');
-	setTimeout(function() {
-		alertBox.classList.toggle('active');
-	}, 2000);
+    var copyPassText = document.getElementById('displayPassword');
+    copyPassText.select();
+    copyPassText.setSelectionRange(0, 9999);
+    document.execCommand('copy');
+    alertBox.classList.toggle('active');
+    setTimeout(function() {
+        alertBox.classList.toggle('active');
+    }, 2000);
 }
 
-
-
-    const currentYear = new Date().getFullYear();
-    document.getElementById('current-year').textContent = currentYear;
-
+const currentYear = new Date().getFullYear();
+document.getElementById('current-year').textContent = currentYear;
